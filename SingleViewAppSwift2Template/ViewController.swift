a741f9b6ee60b097f66a305e4f8fea3aef499faf
Ammanuel Roberts <ammanuelroberts@ammanuels-macbook-air.local>
2016-12-11 12:57:47 -0500

    update

--- SingleViewAppSwift2Template/ViewController.swift
+++ SingleViewAppSwift2Template/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var label3: UILabel!
     @IBOutlet weak var label4: UILabel!
     @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var scoreLabel: UILabel!
     @IBAction func button1() {
     }
     @IBAction func button2() {
     }
+    
     @IBAction func button3() {
     }
     @IBAction func button4() {
@@ -28,8 +28,27 @@ class ViewController: UIViewController {
     }
     @IBAction func button6() {
     }
+ 
     // [Sorts events randomly on labels] Not sure why this isn't working
-    var events: [String] = ["First", "Third", "Second", "Forth"]
+    var allEvents: [String] = ["SOUTHERN STATES PASS LAWS TO DISENFRANCISE BLACKS", "THEODORE ROOSEVELT ELECTED VICE PRESIDENT", "WRIGHT BROTHERS FIRST FLIGHT", "SAN FRANCISCO EARTHQUAKE", "FORD INTRODUCES MODEL T", "WORLD WAR I BEGINS IN EUROPE", "WORLD WAR I ENDS", "TR DIES – CARNEGIE DIES", "GERMAN MONEY HYPER INFLATES", "TREATY OF VERSAILLES, 19TH AMENDMENT", "HINDENBURG ELECTED PRESIDENT OF GERMANY", "STOCK MARKET CRASHES, DEPRESSION BEGINS", "HITLER’S NAZI PARTY GAINS MAJORITY IN PARLIAMENT", "HITLER BEGINS TO TAKE POWER IN GERMANY", "FRANKLIN ROOSEVELT ELECTED PRESIDENT", "FDR INAUGURATED “100 DAYS”", "HINDENBURG DIES, HITLER APPOINTS HIMSELF PRESIDENT  -  “ FUEHRER”", "FDR RE-ELECTED", "German airship HINDENBURG BURNS IN NEW JERSEY", "“MUNICH” – British P.M. Chamberlain agrees that Hitler can have Czechoslovakia.", "Germany invades Holland, Belgium and France – Churchill becomes P.M.", "Germany invades Russia", "JAPANESE ATTACK PEARL HARBOR – U.S. DECLARES WAR ON JAPAN", "HITLER DECLARES WAR ON U.S."]
+    var eventIndex1: Int = 0
+    var eventIndex2: Int = 0
+    var eventIndex3: Int = 0
+    var eventIndex4: Int = 0
+ /****/   while (eventIndex1 == eventIndex2) && (eventIndex2 == eventIndex3) && (eventIndex1 == eventIndex3) && (eventIndex1 == eventIndex4) && (eventIndex2 == eventIndex4) && (eventIndex3 == eventIndex4) {
+        eventIndex1 = GKRandomSource.sharedRandom().nextInt(upperBound: allEvents.count)
+        eventIndex2 = GKRandomSource.sharedRandom().nextInt(upperBound: allEvents.count)
+        eventIndex3 = GKRandomSource.sharedRandom().nextInt(upperBound: allEvents.count)
+        eventIndex4 = GKRandomSource.sharedRandom().nextInt(upperBound: allEvents.count)
+    }
+    var event1 = allEvents[eventIndex1]
+    var event2 = allEvents[eventIndex2]
+    var event3 = allEvents[eventIndex3]
+    var event4 = allEvents[eventIndex4]
+    
+   
+    var events: [String] = [event1, event2, event3, event4]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         var index1: Int = 0
@@ -49,18 +68,16 @@ class ViewController: UIViewController {
         label4.text = events[index4]
     }
    // [Counts down 60 seconds] Working on modifying score accordlny
-    var score = 0
+
+    var timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "counter", userInfo: nil, repeats: true)
     var count = 60
-    var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector(("counter")), userInfo: nil, repeats: true)
     func counter() {
-        
-        if(count > 0)
-        {
+        if(count > 0) {
             count -= 1
             timerLabel.text = String(count)
         } else {
             timer.invalidate()
-            
+            timerLabel.text = "0"
         }
     }
     override func didReceiveMemoryWarning() {
